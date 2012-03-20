9362ff6bf9d45a07e630bcd2805c0555bba78b94
Bill Whitson <wcwhitson@gmail.com>
2012-03-20 01:49:15 +0000

    Modified case statement

--- cookbooks/test/recipes/installcloudoptimizer.rb
+++ cookbooks/test/recipes/installcloudoptimizer.rb
@@ -27,22 +27,18 @@ end
 case node[:platform]
 	when "ubuntu"
 	log "Installing on Ubuntu"
-#	execute "wget" do
-#		command "wget http://apt.cloudopt.com/cloudopt.maverick.list --directory-prefix=/etc/apt/sources.list.d"
-#		action :run
-#	end
-#	execute "wget" do
-#		command "wget http://apt.cloudopt.com/cloudopt.key.asc"
-#		action :run
-#	end
-#	execute "apt-key" do
-#		command "apt-key add ./cloudopt.key.asc"
-#		action :run
-#	end
-#	execute "apt-get"
-#		command "apt-get update"
-#		action :run
-#	end
+	remote_file "/etc/apt/sources.list.d/cloudopt.maverick.list" do
+		source "http://apt.cloudopt.com/cloudopt.maverick.list"
+	end
+	remote_file "/var/tmp/cloudopt.key.asc" do
+		source "http://apt.cloudopt.com/cloudopt.key.asc"
+	end
+	execute "apt-key" do
+		command "apt-key add /var/tmp/cloudopt.key.asc"
+	end
+	execute "apt-get"
+		command "apt-get update"
+	end
 	when "centos"
 	log "Installing on CentOS"
 #	execute "wget" do
