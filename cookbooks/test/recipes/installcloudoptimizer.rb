5fb25ab1efbea8041471ab0e52461abd373ce9f3
Bill Whitson <wcwhitson@gmail.com>
2012-03-20 00:48:53 +0000

    Testing case statement

--- cookbooks/test/recipes/installcloudoptimizer.rb
+++ cookbooks/test/recipes/installcloudoptimizer.rb
@@ -27,36 +27,36 @@ end
 case node[:platform]
 	when "ubuntu"
 	log "Installing on Ubuntu"
-	execute "wget" do
-		command "wget http://apt.cloudopt.com/cloudopt.maverick.list --directory-prefix=/etc/apt/sources.list.d"
-		action :run
-	end
-	execute "wget" do
-		command "wget http://apt.cloudopt.com/cloudopt.key.asc"
-		action :run
-	end
-	execute "apt-key" do
-		command "apt-key add ./cloudopt.key.asc"
-		action :run
-	end
-	execute "apt-get"
-		command "apt-get update"
-		action :run
-	end
+#	execute "wget" do
+#		command "wget http://apt.cloudopt.com/cloudopt.maverick.list --directory-prefix=/etc/apt/sources.list.d"
+#		action :run
+#	end
+#	execute "wget" do
+#		command "wget http://apt.cloudopt.com/cloudopt.key.asc"
+#		action :run
+#	end
+#	execute "apt-key" do
+#		command "apt-key add ./cloudopt.key.asc"
+#		action :run
+#	end
+#	execute "apt-get"
+#		command "apt-get update"
+#		action :run
+#	end
 	when "centos"
 	log "Installing on CentOS"
-	execute "wget" do
-		command "wget https://s3.amazonaws.com/rpm-cloudopt/CloudOpt.selfextracting"
-		action :run
-	end
-	execute "chmod" do
-		command "chmod u+x CloudOpt.selfextracting"
-		action :run
-	end
-	execute "CloudOpt.selfextracting" do
-		command "./CloudOpt.selfextracting"
-		action :run
-	end
+#	execute "wget" do
+#		command "wget https://s3.amazonaws.com/rpm-cloudopt/CloudOpt.selfextracting"
+#		action :run
+#	end
+#	execute "chmod" do
+#		command "chmod u+x CloudOpt.selfextracting"
+#		action :run
+#	end
+#	execute "CloudOpt.selfextracting" do
+#		command "./CloudOpt.selfextracting"
+#		action :run
+#	end
 	else
 	log "Can't install.  This node is neither Ubuntu nor CentOS."
 end
