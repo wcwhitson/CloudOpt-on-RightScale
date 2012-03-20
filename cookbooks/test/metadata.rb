dbfbd865449b8dbbba05616f85c0a67882853cf4
Bill Whitson <wcwhitson@gmail.com>
2012-03-20 17:29:37 +0000

    metadata

--- cookbooks/test/metadata.rb
+++ cookbooks/test/metadata.rb
@@ -1,5 +1,5 @@
-maintainer       "YOUR_COMPANY_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "CloudOpt, Inc."
+maintainer_email "support@cloudopt.com"
 license          "All rights reserved"
 description      "Installs/Configures test"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
@@ -10,5 +10,6 @@ recipe		"test::installcloudoptimizer", "Main installer for cloudoptimizer packag
 attribute "test/configuration/stored/cloudoptimizer.conf",
   :display_name => "Stored configuration",
   :description => "URL to a cloudoptimizer.conf file containing your saved configuration",
-  :required => false,
+  :type => "string",
+  :required => "optional", 
   :recipes => [ "test::installcloudoptimizer" ]
