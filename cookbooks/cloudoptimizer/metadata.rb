86000bb4946bbbc19f47245cd5ad87e2d91bbbef
Bill Whitson <wcwhitson@gmail.com>
2012-05-10 00:27:57 +0000

    Meta data fix

--- cookbooks/cloudoptimizer/metadata.rb
+++ cookbooks/cloudoptimizer/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "support@cloudopt.com"
 license          "All rights reserved"
 description      "Installs/Configures CloudOptimizer"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.1.1"
+version          "0.1.4"
 recipe		"cloudoptimizer::installcloudoptimizer", "Main installer for cloudoptimizer package"
 
 attribute "cloudoptimizer/stored/cloudoptimizer",
