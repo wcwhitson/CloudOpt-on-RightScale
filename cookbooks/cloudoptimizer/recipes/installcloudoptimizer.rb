8f9a0e6fe20ede7a92152056ba8d3ca4eac6c5e3
Bill Whitson <wcwhitson@gmail.com>
2012-05-10 00:00:56 +0000

    Update for CloudOptimizer version 0.9.4.1

--- cookbooks/cloudoptimizer/recipes/installcloudoptimizer.rb
+++ cookbooks/cloudoptimizer/recipes/installcloudoptimizer.rb
@@ -8,6 +8,14 @@ log "========== Beginning CloudOptimizer Installation =========="
 # Prepare EULA Acceptance
 
 log "Noting EULA acceptance."
+
+# The CloudOptimizer package install script checks for the existence of /etc/cloudoptimizer/accept-eula.txt
+# and, if it exists, does not present the EULA acceptance screen.  Here we create the file so that the
+# RightScale automatic installation can proceed.  If the file does not exist when the installation script
+# runs, the RightScale installation will hang.
+
+# Create the /etc/cloudoptimizer directory, since it would not otherwise be created until the package
+# installation script runs.
 directory "/etc/cloudoptimizer" do
 	owner "root"
 	group "root"
@@ -15,6 +23,8 @@ directory "/etc/cloudoptimizer" do
 	action :create
 end
 
+# Create the EULA acceptance file.  The content of the file doesn't matter, but we set it to
+# "RS Accepted EULA" so that we can tell how it was generated.
 file "/etc/cloudoptimizer/accept-eula.txt" do
 	owner "root"
 	group "root"
@@ -26,35 +36,59 @@ end
 # Set up CloudOpt repository
 
 log "Adding CloudOpt software repositories."
+
+# CloudOpt runs on both Ubuntu and CentOS linux, which require different repositories.  Here we detect
+# the linux distribution and then install the appropriate repository.
 case node[:platform]
 when "ubuntu"
 	log "Installing on Ubuntu; using apt repository."
+	# Retrieve and install the repository list
 	remote_file "/etc/apt/sources.list.d/cloudopt.maverick.list" do
 		source "http://apt.cloudopt.com/cloudopt.maverick.list"
 	end
+
+	# Retrieve the repository key file
 	remote_file "/var/tmp/cloudopt.key.asc" do
 		source "http://apt.cloudopt.com/cloudopt.key.asc"
 	end
+
+	# Install the repository key file
 	execute "apt-key" do
 		command "apt-key add /var/tmp/cloudopt.key.asc"
 	end
+
+	# Update the apt database
 	execute "apt-get" do
 		command "apt-get update"
 	end
+
+	# Install rsyslog.  Under certain circumstances, syslog-ng has caused problems with CloudOptimizer.
+	# While those problems are solved at the time this comment was written, they have appeared as regressions
+	# several times.  Installing rsyslog is a risk mitigation measure.  This is necessary only on Ubuntu, as
+	# CentOS uses rsyslog by default.  We may need to reconsider this step if we later provide the ability
+	# to run this RightScript on top of an existing server instance.
 	log "Installing rsyslog for better compatibility on Ubuntu."
 	package "rsyslog"
+
 when "centos"
 	log "Installing on CentOS; using yum repository."
+
+	# Retrieve the repository list
 	remote_file "/var/tmp/CloudOpt.selfextracting" do
 		source "https://s3.amazonaws.com/rpm-cloudopt/CloudOpt.selfextracting"
 		mode "0755"
 	end
+
+	# Install the repository list
 	execute "CloudOpt.selfextracting" do
 		command "/var/tmp/CloudOpt.selfextracting"
 	end
 end
 
 # Set up firewall rules
+
+# This opens the CloudOptimizer ports in iptables.  This step is required for RackSpace instances and should
+# do no harm on others.
 
 log "Modifying firewall rules to allow CloudOptimizer ports."
 if node[:sys_firewall][:enabled] == "enabled"
@@ -64,11 +98,16 @@ if node[:sys_firewall][:enabled] == "enabled"
 	sys_firewall "9003"
 end
 
-# Set up security groups
-
-#if ENV.has_key?('EC2_HOME')
-
 # Install CloudOptimizer
+
+# Here we install and configured the CloudOptimizer packages.  This section must be updated for each new
+# release.  If not updated, the version lock input will not display new versions.  The RightScript will always
+# install the latest version available when the version lock is set to "latest."
+
+# The version locking is kind of a pain the way it is done, but chef doesn't appear to honor architecture
+# in the package command as it should, at least on RightScale.  I wasted a lot of time trying to get it
+# to work before settling on manually specifying the package options.  Thus, we have a lot of manual labor
+# here to determine which package to install when version locking is selected.
 
 if node[:cloudoptimizer][:version] == 'latest'
 	log "Installing the latest cloudoptimizer package."
@@ -77,6 +116,32 @@ else
 	case node[:platform]
 	when "ubuntu"
 		case node[:cloudoptimizer][:version]
+#                when "0.9.4.1"
+#                        case node[:languages][:ruby][:host_cpu]
+#                        when "x86_64"
+#                                package "cloudoptimizer" do
+#                                        version "0.9.4.1"
+#                                        action :install
+#                                end
+#                        when "i686"
+#                                package "cloudoptimizer" do
+#                                        version "0.9.4.1"
+#                                        action :install
+#                                end
+#                        end
+                when "0.9.4"
+                        case node[:languages][:ruby][:host_cpu]
+                        when "x86_64"
+                                package "cloudoptimizer" do
+                                        version "0.9.3.2-53"
+                                        action :install
+                                end
+                        when "i686"
+                                package "cloudoptimizer" do
+                                        version "0.9.4-71"
+                                        action :install
+                                end
+                        end
 		when "0.9.3.2"
 			case node[:languages][:ruby][:host_cpu]
 			when "x86_64"
@@ -133,6 +198,28 @@ else
 		end
 	when "centos"
                 case node[:cloudoptimizer][:version] 
+                when "0.9.4.1"
+                        case node[:languages][:ruby][:host_cpu]
+                        when "x86_64"
+                                execute "yum" do
+                                        command "yum -y install cloudoptimizer-0.9.4.1"
+                                end
+                        when "i686"
+                                execute "yum" do
+                                        command "yum -y install cloudoptimizer-0.9.4.1"
+                                end
+                        end
+                when "0.9.4"
+                        case node[:languages][:ruby][:host_cpu]
+                        when "x86_64"
+                                execute "yum" do
+                                        command "yum -y install cloudoptimizer-0.9.4"
+                                end
+                        when "i686"
+                                execute "yum" do
+                                        command "yum -y install cloudoptimizer-0.9.4"
+                                end
+                        end
                 when "0.9.3.2"
                         case node[:languages][:ruby][:host_cpu]
                         when "x86_64"
@@ -184,6 +271,18 @@ end
 
 # Install additional packages
 
+if node[:cloudoptimizer][:packages][:supplemental][:cloudoptimizer-s3] == 'Install'
+	package "cloudoptimizer-s3"
+end
+
+if node[:cloudoptimizer][:packages][:supplemental][:cloudoptimizer-tools] == 'Install'
+	package "cloudoptimizer-tools"
+end
+
+if node[:cloudoptimizer][:packages][:supplemental][:cloudoptimizer-stat] == 'Install'
+	package "cloudoptimizer-stat"
+end
+
 if node[:cloudoptimizer][:packages][:optional][:vtun] == 'Install'
 	package "vtun"
 end
@@ -224,34 +323,78 @@ else
         end
 end
 
-template "/etc/cloudoptimizer.conf" do
-  source "cloudoptimizer.conf.erb"
-  mode "0644"
-  owner "root"
-  group "root"
-  variables(
-	:home_directory => node[:cloudoptimizer][:configuration][:home_directory],
-	:default_cache_size => node[:cloudoptimizer][:configuration][:default_cache_size],
-	:socket_location => node[:cloudoptimizer][:configuration][:socket_location],
-	:bitmap_size => node[:cloudoptimizer][:configuration][:bitmap_size],
-	:db_memory_size => node[:cloudoptimizer][:configuration][:db_memory_size],
-	:log_directory => node[:cloudoptimizer][:configuration][:log_directory],
-	:log_key => node[:cloudoptimizer][:configuration][:log_key],
-	:compression_engine => node[:cloudoptimizer][:configuration][:compression_engine],
-	:default_compression_level => node[:cloudoptimizer][:configuration][:default_compression_level],
-	:optimistic_deduplication => node[:cloudoptimizer][:configuration][:optimistic_deduplication],
-	:cache_promotion => node[:cloudoptimizer][:configuration][:cache_promotion],
-	:compress_cache => node[:cloudoptimizer][:configuration][:compress_cache],
-	:thread_count => node[:cloudoptimizer][:configuration][:thread_count],
-	:intelligent_mesh => node[:cloudoptimizer][:configuration][:intelligent_mesh],
-	:local_proxy_address => node[:cloudoptimizer][:configuration][:local_proxy_address],
-	:peer_proxy_port => node[:cloudoptimizer][:configuration][:peer_proxy_port],
-	:peer_encryption => node[:cloudoptimizer][:configuration][:peer_encryption],
-	:ssl_key => node[:cloudoptimizer][:configuration][:ssl_key],
-	:ssl_cert => node[:cloudoptimizer][:configuration][:ssl_cert],
-	:ssl_ca => node[:cloudoptimizer][:configuration][:ssl_ca],
-	:peer_statement => node[:cloudoptimizer][:configuration][:peer_statement]
-  )
+# We use chef templates to build the configuration file.  When new options are added to the configuration file, we must
+# add a new template to match.  When multiple versions of the configuration file are supported at the same time, we must
+# determine the CloudOptimizer version that we are installing and use the appropriate template for that version.
+
+# Use the template for CloudOptimizer versions 0.9.3.2 and earlier
+if node[:cloudoptimizer][:version] == '0.9.3.2' || node[:cloudoptimizer][:version] == '0.9.3.1' || node[:cloudoptimizer][:version] == '0.9.3' || node[:cloudoptimizer][:version] == '0.9.2.3'
+	template "/etc/cloudoptimizer.conf" do
+	  source "cloudoptimizer.conf.0.9.4.erb"
+	  mode "0644"
+	  owner "root"
+	  group "root"
+	  variables(
+		:home_directory => node[:cloudoptimizer][:configuration][:home_directory],
+		:default_cache_size => node[:cloudoptimizer][:configuration][:default_cache_size],
+		:socket_location => node[:cloudoptimizer][:configuration][:socket_location],
+		:bitmap_size => node[:cloudoptimizer][:configuration][:bitmap_size],
+		:db_memory_size => node[:cloudoptimizer][:configuration][:db_memory_size],
+		:log_directory => node[:cloudoptimizer][:configuration][:log_directory],
+		:log_key => node[:cloudoptimizer][:configuration][:log_key],
+		:compression_engine => node[:cloudoptimizer][:configuration][:compression_engine],
+		:default_compression_level => node[:cloudoptimizer][:configuration][:default_compression_level],
+		:optimistic_deduplication => node[:cloudoptimizer][:configuration][:optimistic_deduplication],
+		:cache_promotion => node[:cloudoptimizer][:configuration][:cache_promotion],
+		:compress_cache => node[:cloudoptimizer][:configuration][:compress_cache],
+		:thread_count => node[:cloudoptimizer][:configuration][:thread_count],
+		:intelligent_mesh => node[:cloudoptimizer][:configuration][:intelligent_mesh],
+		:local_proxy_address => node[:cloudoptimizer][:configuration][:local_proxy_address],
+		:peer_proxy_port => node[:cloudoptimizer][:configuration][:peer_proxy_port],
+		:peer_encryption => node[:cloudoptimizer][:configuration][:peer_encryption],
+		:ssl_key => node[:cloudoptimizer][:configuration][:ssl_key],
+		:ssl_cert => node[:cloudoptimizer][:configuration][:ssl_cert],
+		:ssl_ca => node[:cloudoptimizer][:configuration][:ssl_ca],
+		:peer_statement => node[:cloudoptimizer][:configuration][:peer_statement]
+	  )
+	end
+# Use the template for version 0.9.4 and later
+else
+        template "/etc/cloudoptimizer.conf" do
+          source "cloudoptimizer.conf.0.9.4.erb"
+          mode "0644"
+          owner "root"
+          group "root"
+          variables(
+                :home_directory => node[:cloudoptimizer][:configuration][:home_directory],
+                :default_cache_size => node[:cloudoptimizer][:configuration][:default_cache_size],
+                :socket_location => node[:cloudoptimizer][:configuration][:socket_location],
+                :bitmap_size => node[:cloudoptimizer][:configuration][:bitmap_size],
+                :db_memory_size => node[:cloudoptimizer][:configuration][:db_memory_size],
+                :log_directory => node[:cloudoptimizer][:configuration][:log_directory],
+                :log_key => node[:cloudoptimizer][:configuration][:log_key],
+                :compression_engine => node[:cloudoptimizer][:configuration][:compression_engine],
+                :default_compression_level => node[:cloudoptimizer][:configuration][:default_compression_level],
+                :optimistic_deduplication => node[:cloudoptimizer][:configuration][:optimistic_deduplication],
+                :cache_promotion => node[:cloudoptimizer][:configuration][:cache_promotion],
+                :compress_cache => node[:cloudoptimizer][:configuration][:compress_cache],
+                :thread_count => node[:cloudoptimizer][:configuration][:thread_count],
+                :intelligent_mesh => node[:cloudoptimizer][:configuration][:intelligent_mesh],
+                :local_proxy_address => node[:cloudoptimizer][:configuration][:local_proxy_address],
+                :peer_proxy_port => node[:cloudoptimizer][:configuration][:peer_proxy_port],
+		:socks_proxy => node[:cloudoptimizer][:configuration][:socks_proxy],
+		:socks_proxy_port => node[:cloudoptimizer][:configuration][:socks_proxy_port],
+		:socks_username => node[:cloudoptimizer][:configuration][:socks_username],
+		:source_transparency => node[:cloudoptimizer][:configuration][:source_transparency], 
+		:transp_int_ip => node[:cloudoptimizer][:configuration][:transp_int_ip],
+		:transp_ext_ip => node[:cloudoptimizer][:configuration][:transp_ext_ip],
+                :peer_encryption => node[:cloudoptimizer][:configuration][:peer_encryption],
+                :ssl_key => node[:cloudoptimizer][:configuration][:ssl_key],
+                :ssl_cert => node[:cloudoptimizer][:configuration][:ssl_cert],
+                :ssl_ca => node[:cloudoptimizer][:configuration][:ssl_ca],
+                :peer_statement => node[:cloudoptimizer][:configuration][:peer_statement]
+          )
+        end
 end
 
 if $reload_config == "yes"
