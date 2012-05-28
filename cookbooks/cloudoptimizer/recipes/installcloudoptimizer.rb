42e900fcf13af545df411bf9d6b176a9b6653fbe
wcwhitson <wcwhitson@gmail.com>
2012-05-27 17:57:57 -0700

    Update cookbooks/cloudoptimizer/recipes/installcloudoptimizer.rb

--- cookbooks/cloudoptimizer/recipes/installcloudoptimizer.rb
+++ cookbooks/cloudoptimizer/recipes/installcloudoptimizer.rb
@@ -65,50 +65,90 @@ log "Adding CloudOpt software repositories."
 
 # CloudOpt runs on both Ubuntu and CentOS linux, which require different repositories.  Here we detect
 # the linux distribution and then install the appropriate repository.
-case node[:platform]
-when "ubuntu"
-  log "Installing on Ubuntu; using apt repository."
-  # Retrieve and install the repository list
-  remote_file "/etc/apt/sources.list.d/cloudopt.maverick.list" do
-    source "http://apt.cloudopt.com/cloudopt.maverick.list"
-  end
+if node[:cloudoptimizer_packages][:special] == 'use tcs' && node[:cloudoptimizer][:version] == 'latest'
+  case node[:platform]
+  when "ubuntu"
+    log "Installing on Ubuntu; using apt repository."
+    log "Installing the test repository.  CloudOpt internal use only."
+    # Retrieve and install the repository list
+    remote_file "/etc/apt/sources.list.d/cloudopt.maverick.list" do
+      source "http://50.16.207.155/cloudopt-test.maverick.list"
+    end
 
-  # Retrieve the repository key file
-  remote_file "/var/tmp/cloudopt.key.asc" do
-    source "http://apt.cloudopt.com/cloudopt.key.asc"
-  end
+    # Retrieve the repository key file
+    remote_file "/var/tmp/cloudopt.tc-key.asc" do
+      source "http://50.16.207.155/cloudopt-test.gpg.key"
+    end
 
-  # Install the repository key file
-  execute "apt-key" do
-    command "apt-key add /var/tmp/cloudopt.key.asc"
-  end
+    # Install the repository key file
+    execute "apt-key" do
+      command "apt-key add /var/tmp/cloudopt.tc-key.asc"
+    end
 
-  # Update the apt database
-  execute "apt-get" do
-    command "apt-get update"
-  end
+    # Update the apt database
+    execute "apt-get" do
+      command "apt-get update"
+    end
 
-  # Install rsyslog.  Under certain circumstances, syslog-ng has caused problems with CloudOptimizer.
-  # While those problems are solved at the time this comment was written, they have appeared as regressions
-  # several times.  Installing rsyslog is a risk mitigation measure.  This is necessary only on Ubuntu, as
-  # CentOS uses rsyslog by default.  We may need to reconsider this step if we later provide the ability
-  # to run this RightScript on top of an existing server instance.
-  log "Installing rsyslog for better compatibility on Ubuntu."
-  package "rsyslog"
+  when "centos"
+    log "Installing on CentOS; using yum repository."
+    log "Installing the test repository.  CloudOpt internal use only."
+    # Retrieve the repository list
+    remote_file "/var/tmp/CloudOpt-Testing.selfextracting" do
+      source "http://50.16.196.194/CloudOpt-Testing.selfextracting"
+      mode "0755"
+    end
 
-when "centos"
-  log "Installing on CentOS; using yum repository."
+    # Install the repository list
+    execute "CloudOpt.selfextracting" do
+      command "/var/tmp/CloudOpt-Testing.selfextracting"
+    end
 
-  # Retrieve the repository list
-  remote_file "/var/tmp/CloudOpt.selfextracting" do
-    source "https://s3.amazonaws.com/rpm-cloudopt/CloudOpt.selfextracting"
-    mode "0755"
-  end
+else
+  case node[:platform]
+  when "ubuntu"
+    log "Installing on Ubuntu; using apt repository."
+    # Retrieve and install the repository list
+    remote_file "/etc/apt/sources.list.d/cloudopt.maverick.list" do
+      source "http://apt.cloudopt.com/cloudopt.maverick.list"
+    end
 
-  # Install the repository list
-  execute "CloudOpt.selfextracting" do
-    command "/var/tmp/CloudOpt.selfextracting"
-  end
+    # Retrieve the repository key file
+    remote_file "/var/tmp/cloudopt.key.asc" do
+      source "http://apt.cloudopt.com/cloudopt.key.asc"
+    end
+
+    # Install the repository key file
+    execute "apt-key" do
+      command "apt-key add /var/tmp/cloudopt.key.asc"
+    end
+
+    # Update the apt database
+    execute "apt-get" do
+      command "apt-get update"
+    end
+
+    # Install rsyslog.  Under certain circumstances, syslog-ng has caused problems with CloudOptimizer.
+    # While those problems are solved at the time this comment was written, they have appeared as regressions
+    # several times.  Installing rsyslog is a risk mitigation measure.  This is necessary only on Ubuntu, as
+    # CentOS uses rsyslog by default.  We may need to reconsider this step if we later provide the ability
+    # to run this RightScript on top of an existing server instance.
+    log "Installing rsyslog for better compatibility on Ubuntu."
+    package "rsyslog"
+
+  when "centos"
+    log "Installing on CentOS; using yum repository."
+
+    # Retrieve the repository list
+    remote_file "/var/tmp/CloudOpt.selfextracting" do
+      source "https://s3.amazonaws.com/rpm-cloudopt/CloudOpt.selfextracting"
+      mode "0755"
+    end
+
+    # Install the repository list
+    execute "CloudOpt.selfextracting" do
+      command "/var/tmp/CloudOpt.selfextracting"
+    end
 end
 
 # Set up firewall rules
