e55445467a383de414dba9e52958a6ce458bc6d2
wcwhitson <wcwhitson@gmail.com>
2012-05-29 12:33:18 -0700

    Update cookbooks/cloudoptimizer/recipes/addsecuritygroup.rb

--- cookbooks/cloudoptimizer/recipes/addsecuritygroup.rb
+++ cookbooks/cloudoptimizer/recipes/addsecuritygroup.rb
@@ -4,12 +4,18 @@
 # Copyright CloudOpt, Inc.  All rights reserved.
 
 require 'ec2'
+require 'lib/ec2/right_ec2.rb'
 
 log "========== Beginning Add Security Group  =========="
 
 if node[:cloudoptimizer][:security][:securitygroup] == "Open CloudOptimizer Ports"
-  cloudopt_group = ec2.security_groups.create('cloudoptimizer')
-  cloudopt_group.authorize_ingress(:tcp, 9001..9003)
+  # Create group
+  @group = 'CloudOptimizer'
+  @ec2.create_security_group(@group,'CloudOptimizer proxy and tunnel ports')
+  group = @ec2.describe_security_groups([@group])[0]
+  # Add ports
+  @ec2.authorize_security_group_named_ingress(@group, account_number, 'default')
+@ec2.authorize_security_group_IP_ingress(@group, 9001,9001,'tcp','0.0.0.0/0')
 end
 
 log "========== Ending Add Security Group  =========="
