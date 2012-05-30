519c9db1c6336cb1f00eb881e0217397f6081430
wcwhitson <wcwhitson@gmail.com>
2012-05-30 11:52:42 -0700

    Update cookbooks/cloudoptimizer/recipes/installcloudoptimizer.rb

--- cookbooks/cloudoptimizer/recipes/installcloudoptimizer.rb
+++ cookbooks/cloudoptimizer/recipes/installcloudoptimizer.rb
@@ -13,15 +13,18 @@ gem_package "pony" do
   action :install
 end
 
+Gem.clear_paths
+require 'pony'
+
 # Provide automatic feedback to cloudopt
 
-#if node[:cloudoptimizer][:automatic_feeback] == "Detailed feedback"
-#  Pony.mail(:to => 'bill@cloudopt.com', :from => 'autofeedback@cloudopt.com', :subject => 'Hello', :body => 'Beginning CloudOptimizer Installation')
-#elsif node[:cloudoptimizer][:automatic_feeback] == "Basic feedback"
-#  Pony.mail(:to => 'bill@cloudopt.com', :from => 'autofeedback@cloudopt.com', :subject => 'Hello', :body => 'Beginning CloudOptimizer Installation with: home_directory #{node[:cloudoptimizer_configuration][:file_locations][:home_directory]} default_cache_size #{node[:cloudoptimizer_configuration][:byte_cache][:default_cache_size]}')
-#else
-#  log "Automatic feedback disabled."
-#end
+if node[:cloudoptimizer][:automatic_feeback] == "Detailed feedback"
+  Pony.mail(:to => 'bill@cloudopt.com', :from => 'autofeedback@cloudopt.com', :subject => 'Hello', :body => 'Beginning CloudOptimizer Installation')
+elsif node[:cloudoptimizer][:automatic_feeback] == "Basic feedback"
+  Pony.mail(:to => 'bill@cloudopt.com', :from => 'autofeedback@cloudopt.com', :subject => 'Hello', :body => 'Beginning CloudOptimizer Installation with: home_directory #{node[:cloudoptimizer_configuration][:file_locations][:home_directory]} default_cache_size #{node[:cloudoptimizer_configuration][:byte_cache][:default_cache_size]}')
+else
+  log "Automatic feedback disabled."
+end
 
 # Install AWS Keys
 
