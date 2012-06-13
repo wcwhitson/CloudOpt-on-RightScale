b763127b3f14b197b12c885049d5198f23182d1a
Bill Whitson <wcwhitson@gmail.com>
2012-06-13 01:13:43 -0700

    Restructure

--- cookbooks/cloudoptimizer/recipes/installcloudoptimizer.rb
+++ cookbooks/cloudoptimizer/recipes/installcloudoptimizer.rb
@@ -231,76 +231,78 @@ log "Fix syslog: Ending"
 log "Repositories: Starting"
 if node[:cloudoptimizer_packages][:special] == 'use tcs' && node[:cloudoptimizer][:version] == 'latest'
   case node[:platform]
-  when "ubuntu"
-    log "Repositories:Installing on Ubuntu; using apt repository."
-    log "Repositories:Installing the test repository.  CloudOpt internal use only."
-    # Retrieve and install the repository list
-    remote_file "/etc/apt/sources.list.d/cloudopt.maverick.list" do
-      source "http://50.16.207.155/cloudopt-test.maverick.list"
+    when "ubuntu"
+      log "Repositories:Installing on Ubuntu; using apt repository."
+      log "Repositories:Installing the test repository.  CloudOpt internal use only."
+      # Retrieve and install the repository list
+      remote_file "/etc/apt/sources.list.d/cloudopt.maverick.list" do
+        source "http://50.16.207.155/cloudopt-test.maverick.list"
+      end
+      # Retrieve the repository key file
+      remote_file "/var/tmp/cloudopt.tc-key.asc" do
+        source "http://50.16.207.155/cloudopt-test.gpg.key"
+      end
+      # Install the repository key file
+      execute "apt-key" do
+        command "apt-key add /var/tmp/cloudopt.tc-key.asc"
+      end
+      # Update the apt database
+      execute "apt-get" do
+        command "apt-get update"
+      end
     end
-    # Retrieve the repository key file
-    remote_file "/var/tmp/cloudopt.tc-key.asc" do
-      source "http://50.16.207.155/cloudopt-test.gpg.key"
-    end
-    # Install the repository key file
-    execute "apt-key" do
-      command "apt-key add /var/tmp/cloudopt.tc-key.asc"
-    end
-    # Update the apt database
-    execute "apt-get" do
-      command "apt-get update"
-    end
-  end
-  when "centos"
-    log "Repositories:Installing on CentOS; using yum repository."
-    log "Repositories:Installing the test repository.  CloudOpt internal use only."
-    # Retrieve the repository list
-    remote_file "/var/tmp/CloudOpt-Testing.selfextracting" do
-      source "http://50.16.196.194/CloudOpt-Testing.selfextracting"
-      mode "0755"
-    end
+    when "centos"
+      log "Repositories:Installing on CentOS; using yum repository."
+      log "Repositories:Installing the test repository.  CloudOpt internal use only."
+      # Retrieve the repository list
+      remote_file "/var/tmp/CloudOpt-Testing.selfextracting" do
+        source "http://50.16.196.194/CloudOpt-Testing.selfextracting"
+        mode "0755"
+      end
 
-    # Install the repository list
-    execute "CloudOpt.selfextracting" do
-      command "/var/tmp/CloudOpt-Testing.selfextracting"
+      # Install the repository list
+      execute "CloudOpt.selfextracting" do
+        command "/var/tmp/CloudOpt-Testing.selfextracting"
+      end
     end
   end
 else
   case node[:platform]
-  when "ubuntu"
-    log "Repositories: Installing on Ubuntu; using apt repository."
-    # Retrieve and install the repository list
-    remote_file "/etc/apt/sources.list.d/cloudopt.maverick.list" do
-      source "http://apt.cloudopt.com/cloudopt.maverick.list"
-    end
+    when "ubuntu"
+      log "Repositories: Installing on Ubuntu; using apt repository."
+      # Retrieve and install the repository list
+      remote_file "/etc/apt/sources.list.d/cloudopt.maverick.list" do
+        source "http://apt.cloudopt.com/cloudopt.maverick.list"
+      end 
 
-    # Retrieve the repository key file
-    remote_file "/var/tmp/cloudopt.key.asc" do
-      source "http://apt.cloudopt.com/cloudopt.key.asc"
-    end
+      # Retrieve the repository key file
+      remote_file "/var/tmp/cloudopt.key.asc" do
+        source "http://apt.cloudopt.com/cloudopt.key.asc"
+      end
 
-    # Install the repository key file
-    execute "apt-key" do
-      command "apt-key add /var/tmp/cloudopt.key.asc"
-    end
+      # Install the repository key file
+      execute "apt-key" do
+        command "apt-key add /var/tmp/cloudopt.key.asc"
+      end
 
-    # Update the apt database
-    execute "apt-get" do
-      command "apt-get update"
+      # Update the apt database
+      execute "apt-get" do
+        command "apt-get update"
+      end
     end
-  end
-  when "centos"
-    log "Repositories: Installing on CentOS; using yum repository."
+    when "centos"
+      log "Repositories: Installing on CentOS; using yum repository."
 
-    # Retrieve the repository list
-    remote_file "/var/tmp/CloudOpt.selfextracting" do
-      source "https://s3.amazonaws.com/rpm-cloudopt/CloudOpt.selfextracting"
-      mode "0755"
-    end
+      # Retrieve the repository list
+      remote_file "/var/tmp/CloudOpt.selfextracting" do
+        source "https://s3.amazonaws.com/rpm-cloudopt/CloudOpt.selfextracting"
+        mode "0755"
+      end
 
-    # Install the repository list
-    execute "CloudOpt.selfextracting" do
-      command "/var/tmp/CloudOpt.selfextracting"
+      # Install the repository list
+      execute "CloudOpt.selfextracting" do
+        command "/var/tmp/CloudOpt.selfextracting"
+      end
     end
   end
 end
