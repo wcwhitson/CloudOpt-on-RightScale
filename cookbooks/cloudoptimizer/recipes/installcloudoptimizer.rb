5ba3a750ffbd47ce868ca5408e9a996ea48af6a5
Bill Whitson <wcwhitson@gmail.com>
2012-06-13 12:56:54 -0700

    Script block fix

--- cookbooks/cloudoptimizer/recipes/installcloudoptimizer.rb
+++ cookbooks/cloudoptimizer/recipes/installcloudoptimizer.rb
@@ -209,20 +209,19 @@ bash "fix_syslog" do
   user "root"
   cwd "/tmp"
   code <<-EOH
-  if [ -f /etc/syslog-ng/syslog-ng.conf ]; then
-    source = `grep -m1 ^source /etc/syslog-ng/syslog-ng.conf |cut -d' ' -f2`
-    cat <<EOF
-    destination d_cloudoptimizer { file("/var/log/cloudoptimizer/cloudoptimizer.log" create_dirs(yes)); };
-    destination d_cloudcopy { file("/var/log/cloudoptimizer/cloudcopy.log" create_dirs(yes)); };
-    destination d_cloudlicense { file("/var/log/cloudoptimizer/cloudlicense.log" create_dirs(yes)); };
-    filter f_cloudoptimizer { level(info..emerg) and program("cloudoptimizer"); };
-    filter f_cloudcopy { level(info..emerg) and program("cloudcopy"); };
-    filter f_cloudlicense { level(info..emerg) and program("cloudlicense"); };
-    log { source(${source}); filter(f_cloudoptimizer); destination(d_cloudoptimizer); };
-    log { source(${source}); filter(f_cloudcopy); destination(d_cloudcopy); };
-    log { source(${source}); filter(f_cloudlicense); destination(d_cloudlicense); };
-    EOF
-    ) >> /etc/syslog-ng/syslog-ng.conf
+  CONF=/etc/syslog-ng/syslog-ng.conf
+  if [ -f "$CONF" ]
+    then
+      source=`grep -m1 ^source ${CONF} |cut -d' ' -f2`
+      echo "destination d_cloudoptimizer { file(\"/var/log/cloudoptimizer/cloudoptimizer.log\" create_dirs(yes)); };" >>$CONF
+      echo "destination d_cloudcopy { file(\"/var/log/cloudoptimizer/cloudcopy.log\" create_dirs(yes)); };" >>$CONF
+      echo "destination d_cloudlicense { file(\"/var/log/cloudoptimizer/cloudlicense.log\" create_dirs(yes)); };" >>$CONF
+      echo "filter f_cloudoptimizer { level(info..emerg) and program(\"cloudoptimizer\"); };" >>$CONF
+      echo "filter f_cloudcopy { level(info..emerg) and program(\"cloudcopy\"); };" >>$CONF
+      echo "filter f_cloudlicense { level(info..emerg) and program(\"cloudlicense\"); };" >>$CONF
+      echo "log { source(${source}); filter(f_cloudoptimizer); destination(d_cloudoptimizer); };" >>$CONF
+      echo "log { source(${source}); filter(f_cloudcopy); destination(d_cloudcopy); };" >>$CONF
+      echo "log { source(${source}); filter(f_cloudlicense); destination(d_cloudlicense); };" >>$CONF
   fi
   EOH
 end
