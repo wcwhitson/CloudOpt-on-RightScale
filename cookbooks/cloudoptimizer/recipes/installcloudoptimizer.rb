9d3e8e2462f894ea6929306fe26749ba6102080e
wcwhitson <wcwhitson@gmail.com>
2012-07-06 17:02:47 -0700

    Update master

--- cookbooks/cloudoptimizer/recipes/installcloudoptimizer.rb
+++ cookbooks/cloudoptimizer/recipes/installcloudoptimizer.rb
@@ -144,8 +144,11 @@ log "Feedback: Ending"
 
 if node[:cloudoptimizer_configuration][:byte_cache][:ebs_volume_size] != '0'
   log "EBS cache volume: creating EBS volume."
-  @ec2   = RightAws::Ec2.new(node[:cloudoptimizer][:cloud_credentials][:aws][:accesskey],node[:cloudoptimizer][:cloud_credentials][:aws][:secretkey])
-  @ec2.create_volume('',50,'us-east-1a')
+  @ec2 = RightAws::Ec2.new(node[:cloudoptimizer][:cloud_credentials][:aws][:accesskey],node[:cloudoptimizer][:cloud_credentials][:aws][:secretkey])
+  volume_name = @ec2.create_volume('',50,'us-east-1a')
+  log "EBS cache volume: created #volume_name->aws_id"
+  attach_status = @ec2.attach_volume('vol-898a6fe0','','/dev/sdh')
+  log "Attached #attach_status->aws_id to #attach_status->aws_instance_id with result #attach_status->aws_status"
 end
 
 ################################################################################
