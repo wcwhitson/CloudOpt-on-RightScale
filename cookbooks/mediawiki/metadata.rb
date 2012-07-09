6ec4dcc0dc14f1e2d139ac64c58975da339f041a
Bill Whitson <wcwhitson@gmail.com>
2012-07-08 18:01:41 -0700

    Mediawiki ServerTemplate

---
+++ cookbooks/mediawiki/metadata.rb
@@ -0,0 +1,159 @@
+maintainer "CloudOpt, Inc."
+maintainer_email "support@cloudopt.com"
+license "All rights reserved"
+description "Installs/Configures/Removes MediaWiki"
+version "0.01"
+recipe "mediawiki::installmediawiki", "Install and configure MediaWiki"
+
+attribute "mediawiki/cloud_credentials/aws/accesskey",
+  :display_name => "AWS Access Key",
+  :description => "Install your AWS access key on the server",
+  :required => "optional",
+  :default => "None",
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/cloud_credentials/aws/secretkey",
+  :display_name => "AWS Secret Key",
+  :description => "Install your AWS secret key on the server",
+  :required => "optional",
+  :default => "None",
+  :recipes => [ "mediawiki::installmediawiki" ]
+  
+attribute "mediawiki/output_compression",
+  :display_name => "Output compression",
+  :description => "Enable or disabled output compression",
+  :required => "optional",
+  :default => "true",
+  :choice => [ "true", "false" ],
+  :recipes => [ "mediawiki::installmediawiki" ]
+      
+attribute "mediawiki/site_name]",
+  :display_name => "Site name",
+  :description => "Name of the MediaWiki installation",
+  :required => "optional",
+  :default => "mediawiki",
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/dns_name",
+  :display_name => "DNS name",
+  :description => "DNS name of the MediaWiki site",
+  :required => "optional",
+  :default => "Public IP",
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/logo_url",
+  :display_name => "Logo URL",
+  :description => "Path to your MediaWiki site logo",
+  :required => "optional",
+  :default => '$wgStylePath/common/images/wiki.png',
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/enable_email",
+  :display_name => "Site email",
+  :description => "Enable or disable site email",
+  :required => "optional",
+  :default => "false",
+  :choice => [ "false", "true" ],
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/enable_user_email",
+  :display_name => "User email",
+  :description => "Enable or disabled user email",
+  :required => "optional",
+  :default => "true",
+  :choice => [ "false", "true" ],
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/admin_email",
+  :display_name => "Administrator e-mail address",
+  :description => "E-mail address of the MediaWiki site administrator",
+  :required => "optional",
+  :default => "apache@Public IP",
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/db_name",
+  :display_name => "Database name",
+  :description => "MediaWiki database name",
+  :required => "optional",
+  :default => "mediawiki",
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/db_root_account",
+  :display_name => "Database root account",
+  :description => "Root account name for the MediaWiki database",
+  :required => "optional",
+  :default => "root",
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/db_root_password",
+  :display_name => "Database root password",
+  :description => "Password for the MediaWiki database root account",
+  :required => "optional",
+  :default => "",
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/db_prefix",
+  :display_name => "Database prefix",
+  :description => "MediaWiki database prefix",
+  :required => "optional",
+  :default => "",
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/file_uploads",
+  :display_name => "File uploads",
+  :description => "Enable or disable file uploads",
+  :required => "optional",
+  :default => "false",
+  :choice => [ "false", "true" ],
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/image_magick",
+  :display_name => "Image Magick",
+  :description => "Enable or disable use of Image Magick to create thumbnails",
+  :required => "optional",
+  :default => "false",
+  :choice => [ "false", "true" ],
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/image_magick_path",
+  :display_name => "Image Magick path",
+  :description => "Path to the Image Magick convert executable",
+  :required => "optional",
+  :default => "/usr/bin/convert",
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/instant_commons",
+  :display_name => "Instant commons",
+  :description => "Enable or disable access to the MediaWiki instant commons",
+  :required => "optional",
+  :default => "false",
+  :choice => [ "false", "true" ],
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/rights_page",
+  :display_name => "Rights page",
+  :description => "Title of the rights page",
+  :required => "optional",
+  :default => "",
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/rights_url",
+  :display_name => "Rights URL",
+  :description => "URL of the rights page",
+  :required => "optional",
+  :default => "",
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/rights_text",
+  :display_name => "Rights text",
+  :description => "MediaWiki rights text",
+  :required => "optional",
+  :default => "",
+  :recipes => [ "mediawiki::installmediawiki" ]
+
+attribute "mediawiki/rights_icon",
+  :display_name => "Rights icon",
+  :description => "Path to the MediaWiki rights icon",
+  :required => "optional",
+  :default => "",
+  :recipes => [ "mediawiki::installmediawiki" ]
---
+++ cookbooks/mediawiki/recipes/installmediawiki.rb
@@ -0,0 +1,106 @@
+#
+# Cookbook Name: Install CloudOptimizer 
+#
+# Copyright CloudOpt, Inc.  All rights reserved.
+
+rightscale_marker :begin
+
+require 'rubygems'
+
+g = gem_package "right_aws" do
+  action :nothing
+end
+g.run_action(:install)
+ 
+Gem.clear_paths
+
+require 'right_aws'
+
+
+################################################################################
+# Install AWS Keys
+################################################################################
+# If the user chooses to provide their AWS credentials, we can pick them up for
+# use with CloudController.  This is less secure than entering the credentials 
+# directly into CloudController, since the credentials are not encrypted.
+################################################################################
+log "AWS Keys: Starting"
+unless node[:mediawiki][:cloud_credentials][:aws][:accesskey] == "None"
+  log "AWS Keys: Installing AWS access key in /root/aws_access_key."
+  file "/root/aws_access_key" do
+    owner "root"
+    group "root"
+    mode "0700"
+    content "#{node[:cloudoptimizer][:cloud_credentials][:aws][:accesskey]}"
+    action :create
+  end
+end
+unless node[:mediawiki][:cloud_credentials][:aws][:secretkey] == "None"
+  log "AWS Keys: Installing AWS secret key in /root/aws_secret_key."
+  file "/root/aws_secret_key" do
+    owner "root"
+    group "root"
+    mode "0700"
+    content "#{node[:cloudoptimizer][:cloud_credentials][:aws][:secretkey]}"
+    action :create
+  end
+end
+log "AWS Keys: Ending"
+
+
+################################################################################
+# Set up repositories
+################################################################################
+# CloudOpt runs on both Ubuntu and CentOS linux, which require different 
+# repositories.  Detect the linux distribution and then install the appropriate 
+# repository.
+################################################################################
+log "Repositories: Starting"
+
+remote_file "/var/tmp/mediawiki-1.19.1.tar.gz" do
+  source "http://download.wikimedia.org/mediawiki/1.19/mediawiki-1.19.1.tar.gz"
+end
+
+execute "tar" do
+  command "tar --strip-components=1 -xvzf /var/tmp/mediawiki-1.19.1.tar.gz -C /var/www/"
+end
+
+log "Repositories: Ending"
+
+
+################################################################################
+# Set defaults to public IP address
+################################################################################
+# 
+################################################################################
+log "Transparent mapping: Starting"
+if node[:mediawiki][:dns_name] == 'Public IP'
+  log "Setting DNS name to #{node[:cloud][:public_ips][0]}"
+  node[:mediawiki][:dns_name] = node[:cloud][:public_ips][0]
+end
+if node[:mediawiki][:admin_email] == 'apache@Public IP'
+  log "Setting admin email to apache@#{node[:cloud][:public_ips][0]}"
+  node[:mediawiki][:admin_email] = node[:cloud][:public_ips][0]
+end
+
+
+################################################################################
+# Build configuration from templates
+################################################################################
+# We use chef templates to build the configuration file.  When new options are 
+# added to the configuration file, we must add a new template to match.  When 
+# multiple versions of the configuration file are supported at the same time, we
+# must determine the CloudOptimizer version that we are installing and use the 
+# appropriate template for that version.
+################################################################################
+log "Template config: Starting"
+  log "Template config: Using template LocalSettings.php.erb."
+  template "/var/www/LocalSettings.php" do
+    source "LocalSettings.php.erb"
+    mode "0644"
+    owner "root"
+    group "root"
+  end
+
+
+rightscale_marker :end
