e4ae9e1c5fdaca17c3a739cfdcf987b10fa99440
Bill Whitson <wcwhitson@gmail.com>
2012-07-20 09:31:46 -0700

    Fix for epel repo versions

--- cookbooks/cloudoptimizer/definitions/fix_epel_repos.rb
+++ cookbooks/cloudoptimizer/definitions/fix_epel_repos.rb
@@ -16,15 +16,27 @@
 define :fix_epel_repos do
   log "Fix EPEL repos: Starting"
   # Delete the existing EPEL repo
-  execute "mv" do
-    command "mv /etc/yum.repos.d/Epel.repo /etc/cloudoptimizer/"
-  end
-  execute "mv" do
-    command "mv /etc/yum.repos.d/RightScale-epel.repo /etc/cloudoptimizer/"
-  end
-  # Install the standard EPEL repo
-  execute "rpm" do
-    command "rpm -Uvh http://dl.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm"
-  end
+  if node[:platform][:version] == '6.0' || node[:platform][:version] == '6.2'
+    execute "mv" do
+      command "mv /etc/yum.repos.d/Epel.repo /etc/cloudoptimizer/"
+    end
+    execute "mv" do
+      command "mv /etc/yum.repos.d/RightScale-epel.repo /etc/cloudoptimizer/"
+    end
+    # Install the standard EPEL repo
+    execute "rpm" do
+      command "rpm -Uvh http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-7.noarch.rpm"
+    end
+  else
+    execute "mv" do
+      command "mv /etc/yum.repos.d/Epel.repo /etc/cloudoptimizer/"
+    end
+    execute "mv" do
+      command "mv /etc/yum.repos.d/RightScale-epel.repo /etc/cloudoptimizer/"
+    end
+    # Install the standard EPEL repo
+    execute "rpm" do
+      command "rpm -Uvh http://dl.fedoraproject.org/pub/epel/5/x86_64/epel-release-5-4.noarch.rpm"
+    end
   log "Fix EPEL repos: Ending"
 end
