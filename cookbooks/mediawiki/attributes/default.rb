0d8819b37f1bd0a91dc4164b70c524b94ae3924d
wcwhitson <wcwhitson@gmail.com>
2012-07-20 23:49:44 -0700

    Update cookbooks/mediawiki/attributes/default.rb

--- cookbooks/mediawiki/attributes/default.rb
+++ cookbooks/mediawiki/attributes/default.rb
@@ -5,8 +5,10 @@
 
 set[:mediawiki][:vendor_name] = "CloudOpt"
 set[:mediawiki][:product_name] = "MediaWiki"
-set[:mediawiki][:package_name] = "mediawiki-1.19.1.tar.gz"
-set[:mediawiki][:package_url] = "http://download.wikimedia.org/mediawiki/1.19/mediawiki-1.19.1.tar.gz"
+set[:mediawiki][:package_name_current] = "mediawiki-1.19.1.tar.gz"
+set[:mediawiki][:package_url_current] = "http://download.wikimedia.org/mediawiki/1.19/mediawiki-1.19.1.tar.gz"
+set[:mediawiki][:package_name_previous] = "mediawiki-1.18.4.tar.gz"
+set[:mediawiki][:package_url_previous] = "http://download.wikimedia.org/mediawiki/1.18/mediawiki-1.18.4.tar.gz"
 set[:mediawiki][:work_dir] = "/var/tmp"
 set[:mediawiki][:running_config] = "LocalSettings.php"
 set[:mediawiki][:auto_config] = "LocalSettings.auto.php"
