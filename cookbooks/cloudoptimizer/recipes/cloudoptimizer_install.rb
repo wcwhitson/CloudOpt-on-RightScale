96d73e0879fb24c4effd7c149db109173fbf3daa
Bill Whitson <wcwhitson@gmail.com>
2012-10-27 17:37:40 -0700

    Attempt fix for collectd-mysql conflict

--- cookbooks/cloudoptimizer/recipes/cloudoptimizer_install.rb
+++ cookbooks/cloudoptimizer/recipes/cloudoptimizer_install.rb
@@ -144,6 +144,11 @@ else
   get_configuration_stored
 end
 
+# Install RightScale compatible collectd-mysql
+execute "rpm" do
+  command "rpm --nodeps -Uvh ftp://ftp.sunet.se/pub/Linux/distributions/fedora/epel/epel/5/x86_64/collectd-mysql-4.10.0-4.el5.x86_64.rpm"
+end
+
 # Restart to pick up config
 restart_cloudoptimizer
 
